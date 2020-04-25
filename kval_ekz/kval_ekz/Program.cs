2abf675336c9ca0b02786361d2f8f8f3ddbe7fca
Johneeeey <nik.gerasimov@inbox.ru>
2020-04-25 09:38:53 +0300

    Создал метод считывания размера массива. Создал массив указанного размера

--- kval_ekz/kval_ekz/Program.cs
+++ kval_ekz/kval_ekz/Program.cs
@@ -8,8 +8,23 @@ namespace kval_ekz
 {
     class Program
     {
+        private static int arrSize = 0;
+        private static Book[] Library;
         static void Main(string[] args)
         {
+            ReadArrSize();
+            Library = new Book[arrSize];
+            Console.ReadKey();
+        }
+        private static void ReadArrSize()
+        {
+            Console.WriteLine("Введите целочисленный размер массива");
+            string read = Console.ReadLine();
+            while(!Int32.TryParse(read, out arrSize))
+            {
+                Console.WriteLine("Некорректный ввод, повторите");
+                read = Console.ReadLine();
+            }
         }
     }
 }
